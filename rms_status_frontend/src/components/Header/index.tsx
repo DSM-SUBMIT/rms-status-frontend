b61a6f4f68b05b5ce245f500c55ecd495161f176
hyejun <hyejun0608@naver.com>
2021-10-22 10:05:21 +0900

    [ADD] Header component

---
+++ rms_status_frontend/src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as S from './style';
+import { PAGE } from '../../constance/header';
+import { openSite } from '../../assets/header';
+
+const Header = () => {
+  const textClickHandler = (event: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
+    const dataId = event.currentTarget.dataset.id;
+    if (dataId === 'user') window.location.href = PAGE[0].uri;
+    else if (dataId === 'admin') window.location.href = PAGE[1].uri;
+  };
+
+  return (
+    <S.Header>
+      <S.HeaderContent>
+        <S.Logo />
+        <div>
+          {PAGE.map(data => {
+            return (
+              <div>
+                <p key={data.id} data-id={data.id} onClick={textClickHandler}>
+                  {data.content}
+                </p>
+                <img src={openSite} data-id={data.id} alt='open-site' onClick={textClickHandler} />
+              </div>
+            );
+          })}
+        </div>
+      </S.HeaderContent>
+    </S.Header>
+  );
+};
+
+export default Header;
---
+++ rms_status_frontend/src/components/Header/style/index.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  width: 100%;
+  min-width: 800px;
+  height: 51px;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderContent = styled.div`
+  width: 800px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    > div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 10px;
+      > p {
+        font-size: 15px;
+        color: #707070;
+        cursor: pointer;
+      }
+      > img {
+        width: 13px;
+        height: 13px;
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+export const Logo = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 1px solid #707070;
+`;
