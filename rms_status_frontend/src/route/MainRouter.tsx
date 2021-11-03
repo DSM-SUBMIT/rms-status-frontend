4d8df8a523129c6390d28e8e5fef13b59d8a50a1
hyejun <hyejun0608@naver.com>
2021-11-03 12:06:25 +0900

    [ADD] error, report path 추가

--- rms_status_frontend/src/route/MainRouter.tsx
+++ rms_status_frontend/src/route/MainRouter.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { FC } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { MainContainer } from '../containers';
+import { MainContainer, ErrorContainer, ReportContainer } from '../containers';
 
 const MainRouter: FC = (): JSX.Element => {
   return (
     <>
       <Switch>
         <Route path='/' component={MainContainer} exact />
+        <Route path='/error' component={ErrorContainer} exact />
+        <Route path='/report' component={ReportContainer} exact />
       </Switch>
     </>
   );
