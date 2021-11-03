49cccbdd76bbfa53e9de27484a8ec8b71caa7e98
hyejun <hyejun0608@naver.com>
2021-11-03 12:07:43 +0900

    [ADD] errorSaga 추가

--- rms_status_frontend/src/modules/redux/middleware/index.ts
+++ rms_status_frontend/src/modules/redux/middleware/index.ts
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import mainSaga from './main';
+import errorSaga from './error';
 
 export default function* rootSaga() {
-  yield all([mainSaga()]);
+  yield all([mainSaga(), errorSaga()]);
 }
