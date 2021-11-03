9afd10656d6990113c47f8a208ac8764dfd26a45
hyejun <hyejun0608@naver.com>
2021-11-03 12:10:36 +0900

    [ADD] 로고 버튼을 눌렀을 때 메인 화면으로 보내기

--- rms_status_frontend/src/components/Header/index.tsx
+++ rms_status_frontend/src/components/Header/index.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import * as S from './style';
 import { PAGE } from '../../constance/header';
 import { openSite } from '../../assets/header';
+import { useHistory } from 'react-router-dom';
 
 const Header = () => {
+  const history = useHistory();
+
   const textClickHandler = (event: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
     const dataId = event.currentTarget.dataset.id;
     if (dataId === 'user') window.location.href = PAGE[0].uri;
     else if (dataId === 'admin') window.location.href = PAGE[1].uri;
+    else if (dataId === 'error') history.push(PAGE[2].uri);
+  };
+
+  const logoClickHandler = () => {
+    history.push('/');
   };
 
   return (
     <S.Header>
       <S.HeaderContent>
-        <S.Logo />
+        <S.Logo onClick={logoClickHandler} />
         <div>
           {PAGE.map(data => {
             return (
