c22da0a01eddbf2bc1a24d2ca92b145fb2c6baa0
hyejun <hyejun0608@naver.com>
2021-11-03 13:20:57 +0900

    [ADD] report reducer 추가

--- rms_status_frontend/src/modules/redux/reducer/index.ts
+++ rms_status_frontend/src/modules/redux/reducer/index.ts
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import MainReducer from './main';
 import ErrorReducer from './error';
+import ReportReducer from './report';
 
-const rootReducer = combineReducers({ main: MainReducer, error: ErrorReducer });
+const rootReducer = combineReducers({
+  main: MainReducer,
+  error: ErrorReducer,
+  report: ReportReducer,
+});
 
 export type reducerType = ReturnType<typeof rootReducer>;
 export default rootReducer;
