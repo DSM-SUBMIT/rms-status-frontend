0e0db39917f31514c20ac5299fee840d6fe1e437
hyejun <hyejun0608@naver.com>
2021-11-14 22:40:23 +0900

    [FIX] 연결 링크 수정

--- rms_status_frontend/src/constance/header/index.ts
+++ rms_status_frontend/src/constance/header/index.ts
@@ -1,5 +1,5 @@
 export const PAGE = [
-  { id: 'user', content: '유저 사이트', uri: 'https://www.naver.com/' },
-  { id: 'admin', content: '어드민 사이트', uri: 'https://www.naver.com/' },
+  { id: 'user', content: '유저 사이트', uri: 'https://user.dsm-rms.com/' },
+  { id: 'admin', content: '어드민 사이트', uri: 'https://admin.dsm-rms.com/' },
   { id: 'error', content: '서비스 장애 기록', uri: '/error' },
 ];
